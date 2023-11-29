4003becaf8e13f6d816babbbf606906c4fd406fa
Neha Sharma <46419950+nehash-1909@users.noreply.github.com>
2023-11-30 00:19:47 +0530

    Create running_sum.c
    Calculating running sums

---
+++ 1)Array/Easy/running_sum.c
@@ -0,0 +1,23 @@
+/*********************************************************************************************************************************
+5. Running Sum of 1d Array
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+*********************************************************************************************************************************/
+
+// Solution
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+
+        for(int i=1;i<nums.size();i++)
+        {
+            nums[i] += nums[i-1];
+        }
+        return nums;
+        
+    }
+};
+
