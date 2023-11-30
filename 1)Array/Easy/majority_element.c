6f0ecab6a2ba5cccd967749ce71d19fbdfc91299
Neha Sharma <46419950+nehash-1909@users.noreply.github.com>
2023-11-30 12:30:26 +0530

    Create majority_element.c
    Find element having frequency more than n/2

---
+++ 1)Array/Easy/majority_element.c
@@ -0,0 +1,33 @@
+/*********************************************************************************************************************************
+7. Majority Element
+
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+*********************************************************************************************************************************/
+
+// Solution
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int selected_ele = 0, votes = 0;
+        for(auto i : nums)
+        {
+            if(votes == 0)
+            {
+                selected_ele = i;
+            }
+            if( selected_ele == i )
+            {
+                votes++;
+            }
+            else
+            {
+                votes--;
+            }
+        }
+        return selected_ele;
+        
+    }
+};
