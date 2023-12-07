4a21d52a67d5213e658717327e8845b827fc9529
Neha Sharma <46419950+nehash-1909@users.noreply.github.com>
2023-12-07 13:39:51 +0530

    Find All Duplicates in an Array

---
+++ 1)Array/Medium/13. allDuplicates.c
@@ -0,0 +1,31 @@
+/*********************************************************************************************************************************
+13. Find All Duplicates in an Array
+
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears once or twice, return an array of all the integers that appears twice.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+
+*********************************************************************************************************************************/
+
+//Solution
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int indx = abs(nums[i]);
+            if(nums[indx-1]<0)
+            {
+                ans.push_back(abs(nums[i]));
+            }
+            nums[indx-1] = nums[indx-1]*-1;
+
+        }
+
+        return ans;
+        
+    }
+};
